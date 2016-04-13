623accfaecbf09ec1a68e3adf3a4d719214a952f
Luis Benet <benet@fis.unam.mx>
2016-04-13 12:41:49 -0500

    Update LICENSE.md, makejl (Documenter.jl) and mkdocs.yml

--- docs/make.jl
+++ docs/make.jl
@@ -2,6 +2,13 @@ using Documenter, TaylorSeries
 
 makedocs(
     modules = TaylorSeries,
-    clean   = true,
+    clean   = false,
     doctest = false,
+)
+
+deploydocs(
+    deps = Deps.pip("pygments", "mkdocs", "mkdocs-bootstrap", "python-markdown-math"),
+    repo   = "github.com/JuliaDiff/TaylorSeries.jl.git",
+    julia  = "0.4",
+    osname = "osx"
 )
