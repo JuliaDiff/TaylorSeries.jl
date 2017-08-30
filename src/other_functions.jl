37fd143af19aecddedf370a21c58cae6d2245b50
Blas Ko <blaskolic@ciencias.unam.mx>
2017-08-30 11:29:15 -0500

    Added function and macro `taylor_expand` for expanding arbitraty functions.

--- src/other_functions.jl
+++ src/other_functions.jl
@@ -126,3 +126,47 @@ function abs{T<:Real}(a::Taylor1{TaylorN{T}})
         (abs(x) is not differentiable at x=0)."""))
     end
 end
+
+#taylor_expand function for Taylor1
+function taylor_expand(f::Function, order::Int64=get_order())
+   a = Taylor1(order)
+   return f(a)
+end
+
+function taylor_expand{T<:Number}(f::Function, x0::T, order::Int64=get_order())
+   a = Taylor1([x0, one(T)], order)
+   return f(a)
+end
+
+#taylor_expand function for TaylorN
+function taylor_expand{T<:Number}(f::Function, x0::Vector{T}, order::Int64=get_order())
+    ll = length(x0)
+    X = set_variables("x",order=order,numvars=ll)
+
+    return f(X.+x0)
+end
+
+#taylor_expand macro for Taylor1
+macro taylor_expand(f, order::Int64=get_order())
+    @eval begin
+       a = Taylor1($order)
+       return $f(a)
+    end
+end
+
+macro taylor_expand(f, x0, order::Int64=get_order())
+    @eval begin
+       a = Taylor1([$x0, one($x0)], $order)
+       return $f(a)
+    end
+end 
+
+#taylor_expandN macro for TaylorN
+macro taylor_expandN(f, x0, order::Int64=get_order())#a Taylor expansion around x0
+    @eval begin
+    ll = length($x0)
+    X = set_variables("x",order=$order,numvars=ll)
+
+    return $f(X.+ $x0)
+    end
+end
