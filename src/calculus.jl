c88340936a4b408782eb6cc7ac5cd1e28b2c017c
Jorge Pérez <jperez@icf.unam.mx>
2017-11-17 17:28:46 -0600

    Add derivative method derivative(n,a) returns a Taylor1 variable; renamed existing derivative(n,a) method as derivativeval

--- src/calculus.jl
+++ src/calculus.jl
@@ -22,11 +22,28 @@ function derivative(a::Taylor1)
 end
 
 """
-    derivative(n, a)
+    derivative(a)
+
+Compute recursively the `Taylor1` polynomial of the n-th derivative of
+`a::Taylor1`.
+"""
+function derivative(n::Int, a::Taylor1{T}) where {T <: Number}
+    @assert a.order ≥ n ≥ 0
+    if n==0
+        return a
+    elseif n==1
+        return derivative(a)
+    else
+        return derivative(n-1, derivative(a))
+    end
+end
+
+"""
+    derivativeval(n, a)
 
 Return the value of the `n`-th derivative of the polynomial `a`.
 """
-function derivative(n::Int, a::Taylor1{T}) where {T<:Number}
+function derivativeval(n::Int, a::Taylor1{T}) where {T<:Number}
     @assert a.order ≥ n ≥ 0
     factorial( widen(n) ) * a[n] :: T
 end
