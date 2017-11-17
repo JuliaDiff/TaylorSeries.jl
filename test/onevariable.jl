45974b83b5bc2e9e5ff133828d76011d9c18a7c7
Jorge Pérez <jperez@icf.unam.mx>
2017-11-17 17:53:42 -0600

    Fix & add derivative tests

--- test/onevariable.jl
+++ test/onevariable.jl
@@ -302,9 +302,12 @@ end
     @test evaluate(v, complex(0.0,0.2)) ==
         [complex(0.0,sinh(0.2)),complex(cos(0.2),sin(-0.2))]
 
-    fifth_exp_deriv = Taylor1(convert(Vector{Float64},exp(ta(1.0))[0:10]))
-    @test derivative(5, exp(ta(1.0))) ≈ exp(1.0) atol=eps() rtol=0.0
-    # @test derivative(3, exp(ta(1.0pi))) == exp(1.0pi)
+    expected_result_approx = Taylor1(convert(Vector{Float64},exp(ta(1.0))[0:10]))
+    @test derivative(5, exp(ta(1.0))) ≈ expected_result_approx atol=eps() rtol=0.0
+    expected_result_approx = Taylor1(convert(Vector{Float64},exp(ta(1.0pi))[0:12]),15)
+    @test derivative(3, exp(ta(1.0pi))) ≈ expected_result_approx atol=eps(16.0) rtol=0.0
+    expected_result_approx = Taylor1(convert(Vector{Float64},exp(ta(1.0pi))[0:5]),15)
+    @test derivative(10, exp(ta(1.0pi))) ≈ expected_result_approx atol=eps(64.0) rtol=0.0
     # @test isapprox(derivative(10, exp(ta(1.0pi))) , exp(1.0pi) )
 
     @test derivative(5, exp(ta(1.0)))() == exp(1.0)
