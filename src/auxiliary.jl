8c29d87d88ad2186ffd53b37a8b45082a0eb7681
Luis Benet <benet@fis.unam.mx>
2018-08-12 00:29:18 +0100

    Solve deprecations warnings due to findfirst and findlast

--- src/auxiliary.jl
+++ src/auxiliary.jl
@@ -223,12 +223,13 @@ end
 
 # Finds the first non zero entry; extended to Taylor1
 function Base.findfirst(a::Taylor1{T}) where {T<:Number}
-    first = findfirst(a.coeffs)
+    first = findfirst(!iszero, a.coeffs)
     isa(first, Nothing) && return -1
     return first-1
 end
+# Finds the last non-zero entry; extended to Taylor1
 function Base.findlast(a::Taylor1{T}) where {T<:Number}
-    last = findlast(a.coeffs)
+    last = findlast(!iszero, a.coeffs)
     isa(last, Nothing) && return -1
     return last-1
 end
