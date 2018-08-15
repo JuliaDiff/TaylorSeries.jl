2fd87f7e1668eee08abde35ee11a904a40ac5d81
Luis Benet <benet@fis.unam.mx>
2018-08-15 15:45:10 -0500

    Amend travis and docs/make.jl to build docs properly

--- docs/make.jl
+++ docs/make.jl
@@ -16,7 +16,7 @@ makedocs(
 deploydocs(
     repo   = "github.com/JuliaDiff/TaylorSeries.jl.git",
     target = "build",
-    julia = "0.6",
+    julia = "0.7",
     osname = "linux",
     deps   = nothing,
     make   = nothing
