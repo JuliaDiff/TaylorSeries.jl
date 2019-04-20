4a2fd5df4f5eed5d6332ad399a08bd06be38c359
Luis Benet <benet@icf.unam.mx>
2019-04-20 11:34:17 -0500

    Add broadcasting.jl, where broadcasting is implemented  for Taylor1

---
+++ src/broadcasting.jl
@@ -0,0 +1,51 @@
+# This file is part of the TaylorSeries.jl Julia package, MIT license
+#
+# Luis Benet & David P. Sanders
+# UNAM
+#
+# MIT Expat license
+#
+
+# Broadcasting: Implemented for Taylor1 and TaylorN
+import .Broadcast: BroadcastStyle, Style, AbstractArrayStyle, Broadcasted
+
+BroadcastStyle(::Type{<:Taylor1}) = Style{Taylor1}()
+BroadcastStyle(::Style{Taylor1}, ::AbstractArrayStyle{0}) = Style{Taylor1}()
+BroadcastStyle(::Style{Taylor1}, ::AbstractArrayStyle{1}) = Style{Taylor1}()
+
+# We follow https://docs.julialang.org/en/v1/manual/interfaces/#man-interface-iteration-1
+"`A = find_taylor(As)` returns the first Taylor1 among the arguments."
+find_taylor(bc::Broadcasted) = find_taylor(bc.args)
+find_taylor(args::Tuple) = find_taylor(find_taylor(args[1]), Base.tail(args))
+find_taylor(x) = x
+find_taylor(a::Taylor1, rest) = a
+find_taylor(::Any, rest) = find_taylor(rest)
+
+# Extend Base.similar
+@eval function Base.similar(bc::Broadcasted{Style{Taylor1}}, ::Type{T}) where {T}
+    # Proper promotion
+    R = Base.Broadcast.combine_eltypes(bc.f, bc.args)
+    # Scan the inputs for the Taylor1:
+    A = find_taylor(bc)
+    # Create the output
+    return Taylor1(similar(A.coeffs, R), A.order)
+end
+
+
+# Adapted from Base.Broadcast.copyto!, base/broadcasting.jl, line 832
+@inline function Base.copyto!(dest::Taylor1, bc::Broadcasted)
+    axes(dest) == axes(bc) || Base.Broadcast.throwdm(axes(dest), axes(bc))
+    # Performance optimization: broadcast!(identity, dest, A) is equivalent to copyto!(dest, A) if indices match
+    if bc.f === identity && bc.args isa Tuple{AbstractArray} # only a single input argument to broadcast!
+        A = bc.args[1]
+        if axes(dest) == axes(A)
+            return copyto!(dest, A)
+        end
+    end
+    bc′ = Base.Broadcast.preprocess(dest.coeffs, bc)
+    # I is the coefficients index
+    @simd for I in eachindex(bc′)
+        @inbounds dest[I-1] = getcoeff(bc′[I], I-1)
+    end
+    return dest
+end
