5945a6ec31da2a7e97bf4b5d221b3450cb89c456
Luis Benet <benet@icf.unam.mx>
2023-01-20 12:22:42 -0600

    Restrict domain consistently of some functions of Taylor series with interval coeffs

--- src/intervals.jl
+++ src/intervals.jl
@@ -9,7 +9,116 @@ function ^(a::Taylor1{T}, n::Integer) where {T<:Interval}
     return power_by_squaring(a, n)
 end
 
+function ^(a::Taylor1{Interval{T}}, r::S) where {T<:Real, S<:Real}
+    a0 = constant_term(a) ∩ Interval(zero(T), T(Inf))
+    aux = one(a0)^r
 
+    iszero(r) && return Taylor1(aux, a.order)
+    aa = one(aux) * a
+    aa[0] = one(aux) * a0
+    r == 1 && return aa
+    r == 2 && return square(aa)
+    r == 1/2 && return sqrt(aa)
+
+    l0 = findfirst(a)
+    lnull = trunc(Int, r*l0 )
+    if (a.order-lnull < 0) || (lnull > a.order)
+        return Taylor1( zero(aux), a.order)
+    end
+    c_order = l0 == 0 ? a.order : min(a.order, trunc(Int,r*a.order))
+    c = Taylor1(zero(aux), c_order)
+    for k = 0:c_order
+        pow!(c, aa, r, k)
+    end
+
+    return c
+end
+
+
+for T in (:Taylor1, :TaylorN)
+    @eval function log(a::$T{Interval{S}}) where {S<:Real}
+        iszero(constant_term(a)) && throw(DomainError(a,
+                """The 0-th order coefficient must be non-zero in order to expand `log` around 0."""))
+        a0 = constant_term(a) ∩ Interval(S(0.0), S(Inf))
+        order = a.order
+        aux = log(a0)
+        aa = one(aux) * a
+        aa[0] = one(aux) * a0
+        c = $T( aux, order )
+        for k in eachindex(a)
+            log!(c, aa, k)
+        end
+        return c
+    end
+
+    @eval function asin(a::$T{Interval{S}}) where {S<:Real}
+        a0 = constant_term(a) ∩ Interval(-one(S), one(S))
+        a0^2 == one(a0) && throw(DomainError(a,
+            """Series expansion of asin(x) diverges at x = ±1."""))
+
+        order = a.order
+        aux = asin(a0)
+        aa = one(aux) * a
+        aa[0] = one(aux) * a0
+        c = $T( aux, order )
+        r = $T( sqrt(1 - a0^2), order )
+        for k in eachindex(a)
+            asin!(c, aa, r, k)
+        end
+        return c
+    end
+
+    @eval function acos(a::$T{Interval{S}}) where {S<:Real}
+        a0 = constant_term(a) ∩ Interval(-one(S), one(S))
+        a0^2 == one(a0) && throw(DomainError(a,
+        """Series expansion of asin(x) diverges at x = ±1."""))
+
+        order = a.order
+        aux = acos(a0)
+        aa = one(aux) * a
+        aa[0] = one(aux) * a0
+        c = $T( aux, order )
+        r = $T( sqrt(1 - a0^2), order )
+        for k in eachindex(a)
+            acos!(c, aa, r, k)
+        end
+        return c
+    end
+
+    @eval function acosh(a::$T{Interval{S}}) where {S<:Real}
+        a0 = constant_term(a) ∩ Interval(one(S), S(Inf))
+        a0^2 == one(a0) && throw(DomainError(a,
+            """Series expansion of acosh(x) diverges at x = ±1."""))
+
+        order = a.order
+        aux = acosh(a0)
+        aa = one(aux) * a
+        aa[0] = one(aux) * a0
+        c = $T( aux, order )
+        r = $T( sqrt(a0^2 - 1), order )
+        for k in eachindex(a)
+            acosh!(c, aa, r, k)
+        end
+        return c
+    end
+
+    @eval function atanh(a::$T{Interval{S}}) where {S<:Real}
+        order = a.order
+        a0 = constant_term(a) ∩ Interval(-one(S), one(S))
+        aux = atanh(a0)
+        aa = one(aux) * a
+        aa[0] = one(aux) * a0
+        c = $T( aux, order)
+        r = $T(one(aux) - a0^2, order)
+        iszero(constant_term(r)) && throw(DomainError(a,
+            """Series expansion of atanh(x) diverges at x = ±1."""))
+
+        for k in eachindex(a)
+            atanh!(c, aa, r, k)
+        end
+        return c
+    end
+end
 
 function evaluate(a::Taylor1, dx::Interval)
     order = a.order
