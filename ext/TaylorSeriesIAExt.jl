e2362d7aa49089ccfc81b2c474da13243741821d
Luis Benet <benet@icf.unam.mx>
2023-12-03 13:02:14 -0600

    Adapt changes in IA

--- ext/TaylorSeriesIAExt.jl
+++ ext/TaylorSeriesIAExt.jl
@@ -10,20 +10,17 @@ isdefined(Base, :get_extension) ? (using IntervalArithmetic) : (using ..Interval
 
 # Method used for Taylor1{Interval{T}}^n
 for T in (:Taylor1, :TaylorN)
-    @eval begin
-        function ^(a::$T{Interval{S}}, n::Integer) where {S<:Real}
-            n == 0 && return one(a)
-            n == 1 && return copy(a)
-            n == 2 && return square(a)
-            n < 0 && return a^float(n)
-            return power_by_squaring(a, n)
-        end
-        ^(a::$T{Interval{S}}, r::Rational) where {S<:Real} = a^float(r)
+    @eval function ^(a::$T{Interval{T}}, n::S) where {T<:Real, S<:Integer}
+        n == 0 && return one(a)
+        n == 1 && return copy(a)
+        n == 2 && return square(a)
+        n < 0 && return a^float(n)
+        return power_by_squaring(a, n)
     end
 end
 
 function ^(a::Taylor1{Interval{T}}, r::S) where {T<:Real, S<:Real}
-    a0 = constant_term(a) ∩ Interval(zero(T), T(Inf))
+    a0 = constant_term(a) ∩ interval(zero(T), T(Inf))
     aux = one(a0)^r
 
     iszero(r) && return Taylor1(aux, a.order)
@@ -47,7 +44,7 @@ function ^(a::Taylor1{Interval{T}}, r::S) where {T<:Real, S<:Real}
     return c
 end
 function ^(a::TaylorN{Interval{T}}, r::S) where {T<:Real, S<:Real}
-    a0 = constant_term(a) ∩ Interval(zero(T), T(Inf))
+    a0 = constant_term(a) ∩ interval(zero(T), T(Inf))
     a0r = a0^r
     aux = one(a0r)
 
@@ -77,7 +74,7 @@ for T in (:Taylor1, :TaylorN)
     @eval function log(a::$T{Interval{S}}) where {S<:Real}
         iszero(constant_term(a)) && throw(DomainError(a,
                 """The 0-th order coefficient must be non-zero in order to expand `log` around 0."""))
-        a0 = constant_term(a) ∩ Interval(S(0.0), S(Inf))
+        a0 = constant_term(a) ∩ interval(zero(S), S(Inf))
         order = a.order
         aux = log(a0)
         aa = one(aux) * a
@@ -90,7 +87,7 @@ for T in (:Taylor1, :TaylorN)
     end
 
     @eval function asin(a::$T{Interval{S}}) where {S<:Real}
-        a0 = constant_term(a) ∩ Interval(-one(S), one(S))
+        a0 = constant_term(a) ∩ interval(-one(S), one(S))
         a0^2 == one(a0) && throw(DomainError(a,
             """Series expansion of asin(x) diverges at x = ±1."""))
 
@@ -107,7 +104,7 @@ for T in (:Taylor1, :TaylorN)
     end
 
     @eval function acos(a::$T{Interval{S}}) where {S<:Real}
-        a0 = constant_term(a) ∩ Interval(-one(S), one(S))
+        a0 = constant_term(a) ∩ interval(-one(S), one(S))
         a0^2 == one(a0) && throw(DomainError(a,
         """Series expansion of asin(x) diverges at x = ±1."""))
 
@@ -124,7 +121,7 @@ for T in (:Taylor1, :TaylorN)
     end
 
     @eval function acosh(a::$T{Interval{S}}) where {S<:Real}
-        a0 = constant_term(a) ∩ Interval(one(S), S(Inf))
+        a0 = constant_term(a) ∩ interval(one(S), S(Inf))
         a0^2 == one(a0) && throw(DomainError(a,
             """Series expansion of acosh(x) diverges at x = ±1."""))
 
@@ -142,7 +139,7 @@ for T in (:Taylor1, :TaylorN)
 
     @eval function atanh(a::$T{Interval{S}}) where {S<:Real}
         order = a.order
-        a0 = constant_term(a) ∩ Interval(-one(S), one(S))
+        a0 = constant_term(a) ∩ interval(-one(S), one(S))
         aux = atanh(a0)
         aa = one(aux) * a
         aa[0] = one(aux) * a0
@@ -179,32 +176,53 @@ function evaluate(a::Taylor1, dx::Interval{S}) where {S<:Real}
     return sum_even + sum_odd*dx
 end
 
-function evaluate(a::TaylorN, dx::IntervalBox{N,T}) where {T<:Real,N}
-    @assert N == get_numvars()
-    a_length = length(a)
-    suma = zero(constant_term(a)) + Interval{T}(0, 0)
+function evaluate(a::TaylorN, dx::AbstractVector{Interval{T}}) where {T<:Real}
+    @assert length(dx) == get_numvars()
+    suma = zero(constant_term(a)) + interval(zero(T))
     @inbounds for homPol in reverse(eachindex(a))
-        suma += evaluate(a[homPol], dx)
+        suma += evaluate(a.coeffs[homPol+1], dx)
     end
 
     return suma
 end
 
-function evaluate(a::HomogeneousPolynomial, dx::IntervalBox{N,T}) where {T<:Real,N}
-    @assert N == get_numvars()
-    dx == IntervalBox(-1..1, Val(N)) && return _evaluate(a, dx, Val(true))
-    dx == IntervalBox( 0..1, Val(N)) && return _evaluate(a, dx, Val(false))
+function evaluate(a::Taylor1{TaylorN{T}}, dx::Interval{S}) where {T<:Real, S<:Real}
+    order = a.order
+    uno = one(dx)
+    dx2 = dx^2
+    if iseven(order)
+        kend = order-2
+        @inbounds sum_even = a[end]*uno
+        @inbounds sum_odd = a[end-1]*zero(dx)
+    else
+        kend = order-3
+        @inbounds sum_odd = a[end]*uno
+        @inbounds sum_even = a[end-1]*uno
+    end
+    @inbounds for k in kend:-2:0
+        sum_odd = sum_odd*dx2 + a[k+1]
+        sum_even = sum_even*dx2 + a[k]
+    end
+    return sum_even + sum_odd*dx
+end
+
+
+function evaluate(a::HomogeneousPolynomial, dx::AbstractVector{Interval{T}}) where {T<:Real}
+    @assert length(dx) == get_numvars()
+    all(dx .== interval(-one(T), one(T))) && return _evaluate(a, dx, Val(true))
+    all(dx .== interval(zero(T), one(T))) && return _evaluate(a, dx, Val(false))
 
     return evaluate(a, dx...)
 end
 
-function _evaluate(a::HomogeneousPolynomial, dx::IntervalBox{N,T}, ::Val{true} ) where {T<:Real,N}
-    a.order == 0 && return a[1] + Interval{T}(0, 0)
+function _evaluate(a::HomogeneousPolynomial, dx::AbstractVector{Interval{T}},
+        ::Val{true} ) where {T<:Real}
+    a.order == 0 && return a[1] + interval(zero(T))
 
     ct = TS.coeff_table[a.order+1]
-    @inbounds suma = a[1]*Interval{T}(0,0)
+    @inbounds suma = a[1]*interval(zero(T))
 
-    Ieven = Interval{T}(0,1)
+    Ieven = interval(zero(T), one(T))
     for (i, a_coeff) in enumerate(a.coeffs)
         iszero(a_coeff) && continue
         if isodd(sum(ct[i]))
@@ -221,8 +239,9 @@ function _evaluate(a::HomogeneousPolynomial, dx::IntervalBox{N,T}, ::Val{true} )
     return suma
 end
 
-function _evaluate(a::HomogeneousPolynomial, dx::IntervalBox{N,T}, ::Val{false} ) where {T<:Real,N}
-    a.order == 0 && return a[1] + Interval{T}(0, 0)
+function _evaluate(a::HomogeneousPolynomial, dx::AbstractVector{Interval{T}},
+        ::Val{false} ) where {T<:Real}
+    a.order == 0 && return a[1] + interval(zero(T))
 
     @inbounds suma = zero(a[1])*dx[1]
     @inbounds for homPol in a.coeffs
@@ -243,14 +262,14 @@ normalize_taylor(a::Taylor1, I::Interval{T}, symI::Bool=true) where {T<:Real} =
     _normalize(a, I, Val(symI))
 
 """
-    normalize_taylor(a::TaylorN, I::IntervalBox, symI::Bool=true)
+    normalize_taylor(a::TaylorN, I::AbstractVector{Interval{T}}, symI::Bool=true)
 
-Normalize `a::TaylorN` such that the intervals in `I::IntervalBox`
+Normalize `a::TaylorN` such that the intervals in `I::AbstractVector{Interval{T}}`
 are mapped by an affine transformation to the intervals `-1..1`
 (`symI=true`) or to `0..1` (`symI=false`).
 """
-normalize_taylor(a::TaylorN, I::IntervalBox{N,T}, symI::Bool=true) where {T<:Real,N} =
-    _normalize(a, I, Val(symI))
+normalize_taylor(a::TaylorN, I::AbstractVector{Interval{T}},
+    symI::Bool=true) where {T<:Real} = _normalize(a, I, Val(symI))
 
 #  I -> -1..1
 function _normalize(a::Taylor1, I::Interval{T}, ::Val{true}) where {T<:Real}
@@ -269,20 +288,20 @@ function _normalize(a::Taylor1, I::Interval{T}, ::Val{false}) where {T<:Real}
 end
 
 #  I -> IntervalBox(-1..1, Val(N))
-function _normalize(a::TaylorN, I::IntervalBox{N,T}, ::Val{true}) where {T<:Real,N}
+function _normalize(a::TaylorN, I::AbstractVector{Interval{T}}, ::Val{true}) where {T<:Real}
     order = get_order(a)
-    x = Vector{typeof(a)}(undef, N)
-    for ind in eachindex(x)
+    x = Vector{typeof(a)}(undef, length(I))
+    @inbounds for ind in eachindex(x)
         x[ind] = mid(I[ind]) + TaylorN(ind, order=order)*radius(I[ind])
     end
     return a(x)
 end
 
 #  I -> IntervalBox(0..1, Val(N))
-function _normalize(a::TaylorN, I::IntervalBox{N,T}, ::Val{false}) where {T<:Real,N}
+function _normalize(a::TaylorN, I::AbstractVector{Interval{T}}, ::Val{false}) where {T<:Real}
     order = get_order(a)
-    x = Vector{typeof(a)}(undef, N)
-    for ind in eachindex(x)
+    x = Vector{typeof(a)}(undef, length(I))
+    @inbounds for ind in eachindex(x)
         x[ind] = inf(I[ind]) + TaylorN(ind, order=order)*diam(I[ind])
     end
     return a(x)
