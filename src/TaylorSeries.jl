c04d9f80018a3cae5980d1a7b0265899d5094d81
Jorge A. Pérez-Hernández <perez.hz@gmail.com>
2024-07-09 15:53:50 +0200

    Update src/TaylorSeries.jl

--- src/TaylorSeries.jl
+++ src/TaylorSeries.jl
@@ -28,8 +28,6 @@ using LinearAlgebra: norm, mul!,
     lu, lu!, LinearAlgebra.lutype, LinearAlgebra.copy_oftype,
     LinearAlgebra.issuccess
 
-using InteractiveUtils
-
 if VERSION >= v"1.7.0-DEV.1188"
     using LinearAlgebra: NoPivot, RowMaximum
 end
