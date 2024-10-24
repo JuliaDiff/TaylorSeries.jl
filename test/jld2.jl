aa788e2063f40d0fd473955705520aa4f1a085bc
Luis Benet <lbenet@users.noreply.github.com>
2024-10-24 16:40:58 -0600

    JLD2 v0.5, and bump patch version (#374)
    * JLD2 v0.5, and bump patch version
    
    Co-authored-by: LuEdRaMo <73906617+LuEdRaMo@users.noreply.github.com>
    
    * CI tests 'lts' (instead of 1.6)
    
    ---------
    
    Co-authored-by: LuEdRaMo <73906617+LuEdRaMo@users.noreply.github.com>

--- test/jld2.jl
+++ test/jld2.jl
@@ -7,7 +7,7 @@ using Test
 
 @testset "Test TaylorSeries JLD2 extension" begin
     dq = set_variables("q", order=4, numvars=6)
-    random_TaylorN = [cos(sum(dq .* rand(6))), sin(sum(dq .* rand(6))), tan(sum(dq .* rand(6)))]
+    random_TaylorN = [exp(sum(rand(6) .* dq)) for _ in 1:10_000]
     jldsave("test.jld2"; random_TaylorN = random_TaylorN)
     recovered_taylorN = JLD2.load("test.jld2", "random_TaylorN")
     @test recovered_taylorN == random_TaylorN
