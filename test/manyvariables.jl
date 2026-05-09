e1535cba7e262c1d92bacbb9585dd06d4c63004e
Luis Eduardo Ramírez Montoya <luiseduardoramirezmontoya@ciencias.unam.mx>
2026-05-09 15:38:10 -0600

    Add hessianmatrix tests

--- test/manyvariables.jl
+++ test/manyvariables.jl
@@ -684,6 +684,13 @@ end
     TS.hessian!(hes1, f1-f2,[1,-1])
     TS.hessian!(hes, g1(xT+1,yT-1)-g2(xT+1,yT-1))
     @test hes1 == hes
+    @test TS.hessianmatrix( f1*f2 )() == TS.hessian( f1*f2 )
+    @test TS.hessianmatrix( f1*f2 ) == TS.hessian( f1*f2, [xT, yT] )
+    @test ([xT yT]*TS.hessianmatrix(f1*f2)*[xT, yT])[1][2] == 2*TaylorN((f1*f2)[2])
+    @test constant_term.(TS.hessianmatrix(f1^2)/2) == TS.hessian(f1^2)/2
+    @test TS.hessianmatrix(f1-f2-2*f1*f2) == (TS.hessianmatrix(f1-f2-2*f1*f2))'
+    @test evaluate.(TS.hessianmatrix(f1-f2), Ref([xT+1,yT-1])) ==
+          TS.hessianmatrix(g1(xT+1,yT-1)-g2(xT+1,yT-1))
 
     use_show_default(true)
     aa = sqrt(2) * xH
